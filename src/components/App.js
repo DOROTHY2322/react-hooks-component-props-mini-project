c76ed46682a575dba61509b3de27174237bf33a7
DOROTHY2322 <kimelidorothy@gmail.com>
2023-01-16 14:51:51 +0300

    complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,10 @@
 import React from "react";
 import blogData from "../data/blog";
+import Header from "./Header";
+import About from "./About";
+import Article from "./Article"
+import ArticleList from "./ArticleList"
+
 
 
 console.log(blogData);
@@ -15,63 +20,3 @@ function App() {
 }
 
 export default App;
-
-function Header(props) {
-  return (
-    <header>
-      <h1>{props.name}</h1>
-    </header>
-  );
-}
-
-
-
-function About(props) {
-  return (
-    <aside>
-      <img src={props.image} alt="blog logo" />
-      <p>{props.text}</p>
-    </aside>
-  );
-}
-
-function ArticleList(props) {
-  return (
-    <main>
-      {props.posts.map((post) => (
-        <Article
-          key={post.id}
-          title={post.title}
-          date={post.date}
-          preview={post.preview}
-          minutes={post.minutes}
-        />
-      ))}
-    </main>
-  );
-}
-
-function Article(props) {
-  let minutesEmoji = ''
-  if(props.minutes<30){
-      const min = Math.ceil(props.minutes/5)
-      for(let i=0;i<min;i++){
-          minutesEmoji += '☕️'
-      }
-      minutesEmoji += ' ' + props.minutes + ' min read'
-  }else {
-      const min = Math.ceil(props.minutes/10)
-      for(let i=0;i<min;i++){
-          minutesEmoji += '🍱'
-      }
-      minutesEmoji += ' ' + props.minutes + ' min read'
-  }
-  return (
-  <article>
-    <h3>{props.title}</h3>
-    <small>{props.date || "January 1, 1970"}</small>
-    <p>{props.preview}</p>
-    <span>{minutesEmoji}</span>
-  </article>
-);
-}
